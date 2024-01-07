e88849bbd06aa931876d4c6d54e7c61428f48654
DamianDuy1302 <damianduy1302@gmail.com>
2024-01-07 16:21:44 +0700

    07/01/24

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,28 +1,78 @@
 import { useEffect, useState } from "react"
 import Product from "./Product";
 import "./style.css"
+import ReactPaginate from 'react-paginate';
 
-const Products = () =>{
+const Products = () => {
+    // const [totalProducts, setTotalProducts] = useState();
+    // const [currentPage, setCurrentPage] = useState(1);
     const [productsList, setProductsList] = useState();
-    useEffect(()=>{
-        const fetchy = async()=>{
+    const [pageCount, setPageCount] = useState()
+    const itemPerPage = 5
+    
+    useEffect(() => {
+        const fetchy = async () => {
             const response = await fetch("http://localhost:3002/products");
             const res = await response.json();
-            if(res){
-                setProductsList(res)
-            }
+            if (res) {
+                fetchy1(1)
+                console.log(res)
+                // setTotalProducts(res.length)
+                
+                setPageCount(parseInt(Math.ceil(res.length/itemPerPage)))
+            }   
         }
         fetchy()
-    }, [productsList])
-    return(
+    }, [])
+
+    const fetchy1 = async (count) => {
+        const response = await fetch(`http://localhost:3002/products?&_page=${count}&_limit=${itemPerPage}`);
+        const res = await response.json();
+        if (res) {
+            setProductsList(res)
+            console.log(res)
+        }
+    }
+    
+
+    const handlePageClick = (e)=>{
+        fetchy1(e.selected+1)
+        // console.log(e.selected+1)
+    }
+    return (
         <>
+
             {productsList && (
                 <div className="productsList">
                     {productsList.map(item => (
-                        <Product key={item.id} item={item}/>
+                        <Product key={item.id} item={item} />
                     ))}
                 </div>
             )}
+
+            
+            <ReactPaginate
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
+
+
         </>
 
     )
